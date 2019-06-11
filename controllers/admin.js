890c4a0b48eb80632c30b347309e06e7048acde1
ping58972 <ping58972@gmail.com>
2019-06-11 16:22:26 -0500

    implement delete product on the client side by using js interact to DOM and doesnot reload the page

--- controllers/admin.js
+++ controllers/admin.js
@@ -178,8 +178,8 @@ exports.getProducts = (req, res, next) => {
     });
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId).then(product => {
         if(!product){
             return next(new Error('Product not found'));
@@ -189,10 +189,27 @@ exports.postDeleteProduct = (req, res, next) => {
     })    
     .then(()=>{
           console.log('DESTROYED PRODUCT');
-          res.redirect('/admin/products');
+          res.status(200).json({message: 'Success!'});
       }).catch(err=> { 
-        const error = new Error(err);
-        error.httpStatusCode = 500;
-        return next(error); 
+       res.status(500).json({message: 'Deleting Product failed.'});
     });
 }
+
+// exports.postDeleteProduct = (req, res, next) => {
+//     const prodId = req.body.productId;
+//     Product.findById(prodId).then(product => {
+//         if(!product){
+//             return next(new Error('Product not found'));
+//         }
+//         fileHelper.deleteFile(product.imageUrl);
+//         return Product.deleteOne({_id: prodId, userId: req.user._id})
+//     })    
+//     .then(()=>{
+//           console.log('DESTROYED PRODUCT');
+//           res.redirect('/admin/products');
+//       }).catch(err=> { 
+//         const error = new Error(err);
+//         error.httpStatusCode = 500;
+//         return next(error); 
+//     });
+// }
